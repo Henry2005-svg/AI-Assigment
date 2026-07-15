21f34c8ef4dfea7a6798ab0ca82176598d68f98a
QiHuai0726 <chuahqh-wm23@student.tarc.edu.my>
2026-07-15 09:33:07 +0800

    Revert "dada"
    This reverts commit 268d5c9eba607bafd365a5d8c078583eea6a4259.

--- AI Assignment/music_playlist_recommender/src/data_loader.py
+++ AI Assignment/music_playlist_recommender/src/data_loader.py
@@ -7,7 +7,7 @@ import pandas as pd
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
 DATA_DIR = PROJECT_ROOT / "data"
 SONGS_CSV = DATA_DIR / "songs.csv"
-INTERACTIONS_CSV = DATA_DIR / "user_interactions.csv" #eason
+INTERACTIONS_CSV = DATA_DIR / "user_interactions.csv"
 
 SONG_COLUMNS = [
     "song_id",
